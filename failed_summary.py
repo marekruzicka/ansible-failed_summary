7d76749fcc6f5fab04b5e535cada2b48e9b99620
Marek Ruzicka <marek.ruzicka@dhl.com>
2025-03-24 11:47:14 +0100

    Format output, add colors

--- failed_summary.py
+++ failed_summary.py
@@ -18,21 +18,28 @@ class CallbackModule(CallbackBase):
         host = result._host.get_name()
         task_name = result.task_name
         if ignore_errors:
-            # Log tasks where ignore_errors was enabled.
             self.ignored_failed_tasks.setdefault(host, []).append(task_name)
         else:
-            # Log tasks that are considered critical failures.
             self.failed_tasks.setdefault(host, []).append(task_name)
 
     def v2_playbook_on_stats(self, stats):
+        # ANSI Color codes
+        RED = '\033[31m'
+        YELLOW = '\033[33m'
+        RESET = '\033[0m'
+
         # Display summary for critical failures.
         if self.failed_tasks:
-            self._display.display("Summary of failed tasks by host (critical failures):")
+            self._display.display("Failed hosts:")
             for host, tasks in self.failed_tasks.items():
-                self._display.display("Host: {} | Failed tasks: {}".format(host, ', '.join(tasks)))
+                message = "{}{}{} | Failed tasks: {}".format(RED, host, RESET, ', '.join(tasks))
+                self._display.display(message)
+            self._display.display("")
+
         # Display summary for failures that were ignored.
         if self.ignored_failed_tasks:
-            self._display.display("Summary of tasks with failures (ignored errors):")
+            self._display.display("Soft failed hosts (errors ignored):")
             for host, tasks in self.ignored_failed_tasks.items():
-                self._display.display("Host: {} | Ignored failed tasks: {}".format(host, ', '.join(tasks)))
-
+                message = "{}{}{} | Ignored failed tasks: {}".format(YELLOW, host, RESET, ', '.join(tasks))
+                self._display.display(message)
+            self._display.display("")
