b47f0b369284a5d26f74e8105a351b9214bb03f0
Marek Ruzicka <marek.ruzicka@dhl.com>
2025-03-24 12:55:11 +0100

    Add support for more colors

--- failed_summary.py
+++ failed_summary.py
@@ -8,6 +8,11 @@ class CallbackModule(CallbackBase):
     CALLBACK_TYPE = 'notification'
     CALLBACK_NAME = 'failed_summary'
 
+
+    # Toggle to enable/disable displaying ignored errors.
+    display_ignored_errors = True
+
+
     def __init__(self):
         super(CallbackModule, self).__init__()
         # Store failures separately: critical failures and ignored failures.
@@ -24,22 +29,31 @@ class CallbackModule(CallbackBase):
 
     def v2_playbook_on_stats(self, stats):
         # ANSI Color codes
-        RED = '\033[31m'
-        YELLOW = '\033[33m'
         RESET = '\033[0m'
+
+        WHITE          = "\033[37m"  # highlight: white
+        BRIGHT_GRAY    = "\033[90m"  # verbose: bright gray
+        BRIGHT_PURPLE  = "\033[95m"  # warn: bright purple
+        RED            = "\033[31m"  # error: red, also used for unreachable and diff_remove
+        DARK_GRAY      = "\033[90m"  # debug: dark gray (using the same as bright gray here)
+        PURPLE         = "\033[35m"  # deprecate: purple
+        CYAN           = "\033[36m"  # skip: cyan, also used for diff_lines
+        GREEN          = "\033[32m"  # ok: green, also used for diff_add
+        YELLOW         = "\033[33m"  # changed: yellow
+        BLUE           = "\033[34m"  # task: blue
 
         # Display summary for critical failures.
         if self.failed_tasks:
             self._display.display("Failed hosts:")
             for host, tasks in self.failed_tasks.items():
-                message = "{}{}{} | Failed tasks: {}".format(RED, host, RESET, ', '.join(tasks))
+                message = "{}{}{} | {}Failed tasks: {}{}".format(RED, host, RESET, BRIGHT_GRAY, RESET, ', '.join(tasks))
                 self._display.display(message)
             self._display.display("")
 
-        # Display summary for failures that were ignored.
-        if self.ignored_failed_tasks:
+        # Display summary for soft failures that were ignored.
+        if self.ignored_failed_tasks and self.display_ignored_errors:
             self._display.display("Soft failed hosts (errors ignored):")
             for host, tasks in self.ignored_failed_tasks.items():
-                message = "{}{}{} | Ignored failed tasks: {}".format(YELLOW, host, RESET, ', '.join(tasks))
+                message = "{}{}{} | {}Ignored failed tasks: {}{}".format(YELLOW, host, RESET, BRIGHT_GRAY, RESET, ', '.join(tasks))
                 self._display.display(message)
             self._display.display("")
